ed0bb0304254a4e11db9ccb79c70340bff268563
TRSiddique <tasfique.cse.21701008@gmail.com>
2025-04-25 08:52:09 +0600

    navbar fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,7 +16,8 @@ const Links=<>
 </>
 
     return (
-        <div className="navbar bg-base-100 shadow-sm justify-between">
+       <div className='bg-white text-black'>
+         <div className="navbar shadow-sm justify-between">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -53,6 +54,7 @@ const Links=<>
   </div>
  </div>
 </div>
+       </div>
     );
 };
 
